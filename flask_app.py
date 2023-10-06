c38303d135b4fbd22f436432537715c8b962ced3
rafachang <108372442+rafachang@users.noreply.github.com>
2023-10-06 16:57:17 -0300

    Database 1

--- flask_app.py
+++ flask_app.py
@@ -15,6 +15,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+db = SQLAlchemy(app)
+
 @app.route('/git_update', methods=['POST'])
 def git_update():
     repo = Repo('./AGSolarEngineers')
@@ -29,5 +31,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    db = SQLAlchemy(app)
     app.run(host='127.0.0.1', port=8000, debug=True)
