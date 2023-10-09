5fcb90fbed7e4b28c2b19217c6ebf500fb4c4b6d
rafachang <108372442+rafachang@users.noreply.github.com>
2023-10-09 17:40:10 -0300

    Bootstrap

--- flask_app.py
+++ flask_app.py
@@ -1,20 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 from git.repo import Repo
 from flask_sqlalchemy import SQLAlchemy
-
+from api import api
 app = Flask(__name__)
-app.config['DEBUG']
 
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="AGSolarEngineers",
-    password="AGSolar2023DB",
-    hostname="AGSolarEngineers.mysql.pythonanywhere-services.com",
-    databasename="AGSolarEngineers$projects",
-)
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
+# SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+#     username="AGSolarEngineers",
+#     password="AGSolar2023DB",
+#     hostname="AGSolarEngineers.mysql.pythonanywhere-services.com",
+#     databasename="AGSolarEngineers$projects",
+# )
+# app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+# app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# db = SQLAlchemy(app)
 
 @app.route('/git_update', methods=['POST'])
 def git_update():
@@ -27,7 +26,13 @@ def git_update():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    print(api.theme)
+    return render_template('index.html', theme=api.theme)
+
+@app.route('/toggle-theme')
+def toggle_theme():
+    api.theme = 'light' if api.theme != 'light' else 'dark'
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8000, debug=True)
