055fd3ea3f804b0870838b5c16be07b2ff3e8809
rafachang <108372442+rafachang@users.noreply.github.com>
2023-10-10 09:34:48 -0300

    Calculo de Concreto feito!

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,4 @@
+import math
 from flask import Flask, redirect, render_template, request, url_for
 from git.repo import Repo
 from flask_sqlalchemy import SQLAlchemy
@@ -55,6 +56,37 @@ def estrutura():
             inverter_table = False
         structure = Estrutura(int(module_length), int(module_amount), int(table_amount), inverter_table)
     return render_template('estrutura.html', default_module_length=default_module_length, default_module_amount=default_module_amount, default_table_amount=default_table_amount, structure=structure, active='Estrutura', theme=api.theme)
+
+@app.route('/concreto', methods=('GET', 'POST'))
+def concreto():
+    default_base_amount = 0
+    default_height_buried = 1.6
+    default_ray_buried = 0.15
+    default_height_exposed = 0.4
+    default_ray_exposed = 0.2
+
+    if request.method == "POST":
+        base_amount = request.form['txt_base_amount']
+        default_base_amount = float(base_amount)
+        height_buried = request.form['txt_height_buried']
+        default_height_buried = float(height_buried)
+        ray_buried = request.form['txt_ray_buried']
+        default_ray_buried = float(ray_buried)
+        height_exposed = request.form['txt_height_exposed']
+        default_height_exposed = float(height_exposed)
+        ray_exposed = request.form['txt_ray_exposed']
+        default_ray_exposed = float(ray_exposed)
+
+    default_volume_buried = math.pi*(default_ray_buried**2.0)*default_height_buried
+    default_volume_exposed = math.pi*(default_ray_exposed**2.0)*default_height_exposed
+    default_volume_per_base = default_volume_buried+default_volume_exposed
+    default_volume_total = math.ceil(default_base_amount*default_volume_per_base*1.1)
+    default_feature = "H21"
+        
+    return render_template("concreto.html", active='Estrutura', default_base_amount=default_base_amount,
+                            default_height_buried=default_height_buried, default_ray_buried=default_ray_buried, default_volume_buried=default_volume_buried, 
+                            default_height_exposed=default_height_exposed, default_ray_exposed=default_ray_exposed, default_volume_exposed=default_volume_exposed, 
+                            default_volume_per_base=default_volume_per_base, default_volume_total=default_volume_total, default_feature=default_feature, theme=api.theme)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8000, debug=True)
